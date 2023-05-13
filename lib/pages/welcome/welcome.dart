802468d06a73ed4275e0e6a81f9258cb0988117f
Arllain <arllain@gmail.com>
2023-05-13 12:37:17 -0300

    using MultiBlocProvider

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_learning_app/main.dart';
 import 'package:flutter_learning_app/pages/welcome/bloc/welcome_bloc.dart';
 import 'package:flutter_learning_app/pages/welcome/bloc/welcome_states.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -15,6 +16,7 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController _pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,6 +31,7 @@ class _WelcomeState extends State<Welcome> {
                 alignment: Alignment.topCenter,
                 children: [
                   PageView(
+                    controller: _pageController,
                     onPageChanged: (index) {
                       BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
                       state.page = index;
@@ -112,31 +115,43 @@ class _WelcomeState extends State<Welcome> {
                 fontWeight: FontWeight.normal),
           ),
         ),
-        Container(
-            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
-            width: 325.w,
-            height: 50.h,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(15.w)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 1,
-                  blurRadius: 2,
-                  offset: const Offset(0, 1),
-                )
-              ],
-            ),
-            child: Center(
-              child: Text(
-                buttonName,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.normal),
+        GestureDetector(
+          onTap: () {
+            if (index < 3) {
+              _pageController.animateToPage(index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.decelerate);
+            } else {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const MyHomePage()));
+            }
+          },
+          child: Container(
+              margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+              width: 325.w,
+              height: 50.h,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1),
+                  )
+                ],
               ),
-            ))
+              child: Center(
+                child: Text(
+                  buttonName,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.normal),
+                ),
+              )),
+        )
       ],
     );
   }
